e7e50763ca0b8850cb007064d8910d7781c8d63c
Korobenko Vik <korobenko.vik@gmail.com>
2024-04-22 23:40:19 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,23 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
-const Card = () => {
-  return (
-    <div>Hello world!</div>
-  )
-};
-
-
-  
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Card/>);
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App/>
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  </React.StrictMode>
+);
